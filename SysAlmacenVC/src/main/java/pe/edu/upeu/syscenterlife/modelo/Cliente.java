4ecb1e9dfaee72b1b8de198a518b46f857dbe23b
davidmp <mamanipari@gmail.com>
2024-05-20 08:56:22 -0500

    Modelo de Base de Datos a nivel de objetos

--- SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Cliente.java
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/Cliente.java
@@ -1,7 +1,13 @@
 package pe.edu.upeu.syscenterlife.modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +23,9 @@ public class Cliente {
    String dniruc;
    String nombrers;
    String documento;
+   
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "dniruc", referencedColumnName = "dniruc")
+    @JsonIgnoreProperties({"dniruc"})
+    public List<Venta> ventas;
 }
